d7abd2671a787d1175d2a2b677ac3969e112d82c
Fearless <zero75elta@gmail.com>
2026-03-05 01:12:47 +0100

    Restore tailwindcss plugin in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// On enlève l'import de tailwind temporairement pour que npx puisse se lancer
+// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
